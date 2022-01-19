ac32a39c376cc6ad6b8a1b5a1aeb4391afe4058a
Gubaratalla <97310360+Gubaratalla@users.noreply.github.com>
2022-01-19 12:13:36 +0400

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,18 @@
+var video = document.getElementById('myVideo'); 
+
+
+playBtn.addEventListener('click',function play() {video.play();
+
+})
+
+pauseBtn.addEventListener('click',function pause() {video.pause();
+
+})
+
+
+// video.addEventListener('',function(){ video.play(); });
+
+//video.play();
+
+
+//video.addEventListener('',function(){ video.play(); });
