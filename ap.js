2bd1a0361dede5861b80ce60d61a0406c03bb58e
Gubaratalla <97310360+Gubaratalla@users.noreply.github.com>
2022-01-21 05:26:14 +0400

    Update ap.js

--- ap.js
+++ ap.js
@@ -9,13 +9,13 @@ pauseBtn.addEventListener('click',function pause() {video.pause();
 
 })
 
-p2Btn.addEventListener('click',function other() {
-  if(windows.location.href=='index.html')
-  {
+// p2Btn.addEventListener('click',function other() {
+//   if(windows.location.href=='index.html')
+//   {
   window.location.href = 'index1.html';
-  }
-  else 
-         window.location.href = 'index.html';
+//   }
+//   else 
+//          window.location.href = 'index.html';
 })
 
 // video.addEventListener('',function(){ video.play(); });
