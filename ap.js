ef4806756207c6da95948aacf96b603919ba5f71
Gubaratalla <97310360+Gubaratalla@users.noreply.github.com>
2022-01-21 05:27:17 +0400

    Update ap.js

--- ap.js
+++ ap.js
@@ -9,7 +9,7 @@ pauseBtn.addEventListener('click',function pause() {video.pause();
 
 })
 
-// p2Btn.addEventListener('click',function other() {
+ p2Btn.addEventListener('click',function other() {
 //   if(windows.location.href=='index.html')
 //   {
   window.location.href = 'index1.html';
